9c34bd9312f7e966ec546f6027965c99dbbfa31d
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-21 08:20:53 +0530

    first commit

---
+++ Project04_check entered number is odd or even/Project04_check entered number is odd or even/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project04_check_entered_number_is_odd_or_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0) // Even number if remainder is 0
+                Console.WriteLine("Entered Number is an Even Number");
+            else
+                Console.WriteLine("Entered Number is odd Number");
+
+            Console.ReadLine();
+        }
+    }
+}
